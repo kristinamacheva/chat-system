a6530ddb024be7e6e2172c8c9ec01f331e495f42
kristinamacheva <macheva.kristina@gmail.com>
2024-12-26 17:32:58 +0200

    Add global CORS configuration

---
+++ backend/src/main/java/com/example/backend/config/WebConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration class for setting up web-related configurations in the application.
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    /**
+     * Configures CORS mappings for the application.
+     * This allows requests from any origin, with any HTTP method and any headers.
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
